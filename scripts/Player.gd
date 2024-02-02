93c0f7779fd879ad3b65add8593b1a0995b65029
3soma <laura.s.lumme@gmail.com>
2024-02-02 23:11:24 +0200

    Made dropping use position manipulation instead of gravity

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,6 +4,8 @@ export(float) var vertical_distance_above_tower = 400
 export(float) var move_speed = 400
 export(float) var rotation_speed = 200
 export(float, 0, 400, 1) var horizontal_movement_limit = 200
+export(float) var block_drop_speed = 100
+export(float) var fast_block_drop_speed = 200
 var cursor_sprite:Sprite
 var active_block:Block setget set_active_block, get_active_block
 var my_tower:Tower
@@ -16,6 +18,10 @@ func _process(delta):
 	position.x += movement_input() * move_speed * delta
 	position.x = clamp(position.x, -horizontal_movement_limit, horizontal_movement_limit)
 	active_block.global_position.x = lerp(active_block.global_position.x, global_position.x, 0.5)
+	if Input.is_action_pressed("move_down"):
+		active_block.global_position.y += fast_block_drop_speed * delta
+	else:
+		active_block.global_position.y += block_drop_speed * delta
 	active_block.rotate_block(rotation_input() * rotation_speed * delta)
 	
 func move_to_height(var tower_height:float) -> void:
@@ -41,9 +47,19 @@ func rotation_input() -> float:
 
 func set_active_block(var new_active_block:Block) -> Block:
 	if active_block:
-		pass
+		enable_gravity_for_active_block()
 	active_block = new_active_block
+	disable_gravity_for_active_block()
 	return active_block
 	
 func get_active_block() -> Block:
 	return active_block
+
+var active_block_true_gravity_scale:float
+func enable_gravity_for_active_block():
+	active_block.set_sleeping(false) 
+	active_block.gravity_scale = active_block_true_gravity_scale
+func disable_gravity_for_active_block():
+	active_block.set_sleeping(false)
+	active_block_true_gravity_scale = active_block.gravity_scale
+	active_block.gravity_scale = 0.0
