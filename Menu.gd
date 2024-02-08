5853c5f18af14e29d4a80060061bb1a3fcfa433a
unknown <nurmiaho.elma@gmail.com>
2024-02-08 20:17:18 +0200

    feat: main menu

---
+++ Menu.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export var mainGameScene : PackedScene
+
+func _on_PlayButton_button_up():
+	get_tree().change_scene(mainGameScene.resource_path)
+
+func _on_QuitButton_button_up():
+	get_tree().quit()
